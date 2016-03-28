16154ba130a9c3c3dcc31d9929235c6226957478
Zach <zstecker@princeton.edu>
2016-03-27 23:37:46 -0400

    initial iOS commit

---
+++ Pitchslapp/Pitchslapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitchslapp
+//
+//  Created by Zachary Stecker on 3/27/16.
+//  Copyright © 2016 Social Coderz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
